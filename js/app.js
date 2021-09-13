5b93e155ad4118129c0a79351ac22ccfe0611272
Farshid Ahsan <arnob.farshid@gmail.com>
2021-09-13 16:36:07 +0600

    rating added

--- js/app.js
+++ js/app.js
@@ -20,8 +20,10 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <h2>Rating:${product.rating.rate}</h2>
+      <h2>How Many People Give this Rating:${product.rating.count}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="detailsProduct()" id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
@@ -84,3 +86,28 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+// const detailsProduct = () => {
+//   const div = document.createElement('div')
+//   div.innerHTML = `
+//  <div class="modal" tabindex="-1">
+//   <div class="modal-dialog">
+//     <div class="modal-content">
+//       <div class="modal-header">
+//         <h5 class="modal-title">Modal title</h5>
+//         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+//       </div>
+//       <div class="modal-body">
+//         <p></p>
+//       </div>
+//       <div class="modal-footer">
+//         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+//         <button type="button" class="btn btn-primary">Save changes</button>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+
+//  `
+//   document.getElementById('show-details').appendChild(div)
+// }
